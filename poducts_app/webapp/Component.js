4d4f01f6c9f551586acc0b735af114b405af131c
LEVERX\katsiaryna.tabunova <katepilipionak18.90@gmail.com>
2024-04-22 21:21:31 +0300

    First commit

---
+++ poducts_app/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.edu.products.poductsapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
